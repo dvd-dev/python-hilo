b60f14c2d19107da0caa69a59dec24fc2c384555
Ian C. <108159253+ic-dev21@users.noreply.github.com>
2025-02-09 20:35:09 -0500

    Linting

--- pyhilo/oauth2helper.py
+++ pyhilo/oauth2helper.py
@@ -4,7 +4,6 @@ import os
 import re
 from typing import Any, cast
 
-
 from pyhilo.const import (
     AUTH_AUTHORIZE,
     AUTH_CHALLENGE_METHOD,
@@ -12,6 +11,7 @@ from pyhilo.const import (
     AUTH_SCOPE,
     AUTH_TOKEN,
 )
+
 
 class OAuth2Helper:
     """Custom OAuth2 implementation."""
@@ -36,7 +36,7 @@ class OAuth2Helper:
             "code_challenge": self._code_challenge,
             "code_challenge_method": AUTH_CHALLENGE_METHOD,
             "response_type": "code",
-            "client_id": AUTH_CLIENT_ID
+            "client_id": AUTH_CLIENT_ID,
         }
 
     def get_token_request_parameters(self, code, redirect_uri):
@@ -46,5 +46,3 @@ class OAuth2Helper:
             "redirect_uri": redirect_uri,
             "code_verifier": self._code_verifier,
         }
-
-
