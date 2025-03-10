37277ed67be4cef8ee95fda2d75001cab4585c09
Abigail Asselin <abigail.asselin@hiloenergie.com>
2025-03-09 21:21:54 -0400

    Enhance GraphQL value mapping with additional device types and attributes

--- pyhilo/device/graphql_value_mapper.py
+++ pyhilo/device/graphql_value_mapper.py
@@ -25,29 +25,51 @@ class GraphqlValueMapper:
         match device["deviceType"]:
             case "Tstat":
                 attributes.extend(self._build_thermostat(device))
-                return self._map_to_device_reading(attributes)
-            # case "CCE":  # Water Heater
-            #     attributes.extend(self._build_water_heater(device))
-            #     return self._map_to_device_reading(attributes)
-            # case "CCR":  # ChargeController
-            #     attributes.extend(self._build_charge_controller(device))
-            #     return self._map_to_device_reading(attributes)
-            # case "HeatingFloor":
+            case "CCE":  # Water Heater
+                 attributes.extend(self._build_water_heater(device))
+            case "CCR":  # ChargeController
+                attributes.extend(self._build_charge_controller(device))
+            case "HeatingFloor":
+                 attributes.extend(self._build_floor_thermostat(device))
             # case "LowVoltageTstat":
             # case "ChargingPoint":
-            # case "Meter":
-            # case "Hub": # Gateway
-            # case "ColorBulb":
-            # case "Dimmer":
-            # case "Switch":
+            case "Meter":
+                attributes.extend(self._build_smart_meter(device))
+            case "Hub": # Gateway
+                attributes.extend(self._build_gateway(device))
+            case "ColorBulb":
+                attributes.extend(self._build_light(device))
+            case "Dimmer":
+                attributes.extend(self._build_dimmer(device))
+            case "Switch":
+                attributes.extend(self._build_switch(device))
             case _:
-                # Add the default logic here if needed
                 pass
+        return self._map_to_device_reading(attributes)
 
     def _map_to_device_reading(
         self, attributes: list[Dict[str, Any]]
     ) -> list[DeviceReading]:
         return [DeviceReading(**attr) for attr in attributes]
+
+    def _build_smart_meter(self, device: Dict[str, Any]) -> list[Dict[str, Any]]:
+        return [
+            self.build_attribute(device["hiloId"], "LastUpdate", device["LastUpdate"]),
+            self.build_attribute(device["hiloId"], "ZigbeeChannel", device["zigbeeChannel"]),
+            self.build_attribute(device["hiloId"], "Disconnected", device["IsDisconnected"]), #??? gateway value??
+            self._map_power(device),
+        ]
+
+    def _build_gateway(self, device: Dict[str, Any]) -> list[Dict[str, Any]]:
+        return [
+            self.build_attribute(device["hiloId"], "LastStatusTime", device["LastConnectionTime"]),
+            self.build_attribute(device["hiloId"], "Version", device["controllerSoftwareVersion"]),
+            self.build_attribute(device["hiloId"], "Disconnected", device["connectionStatus"] == 2), # Offline
+            self.build_attribute(device["hiloId"], "ZigbeePairingActivated", device["zigBeePairingMode"]),
+            self.build_attribute(device["hiloId"], "ZigbeeChannel", device["zigbeeChannel"]),
+            self.build_attribute(device["hiloId"], "WillBeConnectedToSmartMeter", device["willBeConnectedToSmartMeter"]),
+            self.build_attribute(device["hiloId"], "SmartMeterUnpaired", device["smartMeterPairingStatus"]),
+        ]
 
     def _build_thermostat(self, device: Dict[str, Any]) -> list[Dict[str, Any]]:
         return [
@@ -62,12 +84,6 @@ class GraphqlValueMapper:
                 device["ambientTempSetpoint"]["value"],
             ),
             self.build_attribute(device["hiloId"], "HeatDemand", device["heatDemand"]),
-            self.build_attribute(
-                device["hiloId"],
-                "Heating",
-                device["power"]["value"] != "0" or device["power"]["value"] is not None,
-            ),
-            self.build_attribute(device["hiloId"], "Power", device["power"]["value"]),
             self.build_attribute(device["hiloId"], "Version", device["version"]),
             self.build_attribute(
                 device["hiloId"], "ZigbeeVersion", device["zigbeeVersion"]
@@ -78,51 +94,75 @@ class GraphqlValueMapper:
             self.build_attribute(
                 device["hiloId"], "ThermostatAllowedModes", device["allowedModes"]
             ),
+            self._map_power(device),
+            self._map_heating(device),
             self.build_attribute(device["hiloId"], "ThermostatMode", device["mode"]),
             self._map_gd_state(device),
         ]
 
+    def _build_floor_thermostat(self, device: Dict[str, Any]) -> list[Dict[str, Any]]:
+        attributes = self._build_thermostat(device)
+        return attributes.extend(
+            self.build_attribute(
+                device["hiloId"], "FloorMode", self._map_to_floor_mode(device["floorMode"])
+            ),
+            self.build_attribute(
+                device["hiloId"], "FloorLimit", device["FloorLimit"]["value"]
+            ),
+        )
+
     def _build_water_heater(self, device: Dict[str, Any]) -> list[Dict[str, Any]]:
         attributes = self._build_charge_controller(device)
-        attributes.extend(
+        return attributes.extend(
             [
                 self.build_attribute(
-                    device["hiloId"], "Power", device["power"]["value"]
+                    device["hiloId"], "AbnormalTemperature", device["alerts"].contains("30") # AbnormalTemperature alert
                 ),
-                self.build_attribute(
-                    device["hiloId"], "GdState", device["gDState"] == "Active"
+                 self.build_attribute(
+                    device["hiloId"], "CurrentTemperature", device["ProbeTemperature"]["value"]# AbnormalTemperature alert
                 ),
-                self.build_attribute(device["hiloId"], "Version", device["version"]),
-                self.build_attribute(
-                    device["hiloId"], "ZigbeeVersion", device["zigbeeVersion"]
-                ),
-                self.build_attribute(
-                    device["hiloId"], "Alerts", device["ambientHumidity"]
-                ),
-                self.build_attribute(
-                    device["hiloId"], "ThermostatAllowedModes", device["allowedModes"]
-                ),
-                self.build_attribute(
-                    device["hiloId"], "ThermostatMode", device["mode"]
-                ),
-                self.build_attribute(
-                    device["hiloId"],
-                    "Disconnected",
-                    device["connectionStatus"] == "Disconnected",
-                ),
-                self.build_attribute(device["hiloId"], "Unpaired", False),
+                self._map_heating(device),
             ]
         )
 
     def _build_charge_controller(self, device: Dict[str, Any]) -> list[Dict[str, Any]]:
         return [
-            self.build_attribute(device["hiloId"], "Power", device["power"]["value"]),
+            self._map_power(device),
             self.build_attribute(device["hiloId"], "Version", device["version"]),
             self.build_attribute(
                 device["hiloId"], "ZigbeeVersion", device["zigbeeVersion"]
             ),
             self._map_gd_state(device),
             self._map_drms_state(device),
+            self.build_attribute(
+                    device["hiloId"], "CcrAllowedModes", device["ccrAllowedModes"]
+            ),
+            self.build_attribute(
+                    device["hiloId"], "CcrMode", device["ccrMode"]
+            ),
+        ]
+
+    def _build_switch(self, device: Dict[str, Any]) -> list[Dict[str, Any]]:
+        return [
+            self._map_power(device),
+            self.build_attribute(device["hiloId"], "Status", device["State"]),
+            self.build_attribute(device["hiloId"], "OnOff", device["State"]),
+        ]
+
+    def _build_dimmer(self, device: Dict[str, Any]) -> list[Dict[str, Any]]:
+        return [
+            self._map_power(device),
+            self.build_attribute(device["hiloId"], "Intensity", device["Level"]["value"]/100),
+            self.build_attribute(device["hiloId"], "OnOff", device["State"]),
+        ]
+
+    def _build_light(self, device: Dict[str, Any]) -> list[Dict[str, Any]]:
+        return [
+            self.build_attribute(device["hiloId"], "ColorTemperature", device["ColorTemperature"]["value"]),
+            self.build_attribute(device["hiloId"], "Intensity", device["Level"]["value"]/100),
+            self.build_attribute(device["hiloId"], "OnOff", device["State"]),
+            self.build_attribute(device["hiloId"], "Hue", device.get("Hue") or 0),
+            self.build_attribute(device["hiloId"], "Saturation", device.get("Saturation") or 0),
         ]
 
     def _map_basic_device(self, device: Dict[str, Any]) -> list[Dict[str, Any]]:
@@ -146,7 +186,23 @@ class GraphqlValueMapper:
             self.build_attribute(
                 device["hiloId"], "DrmsState", device["gDState"] == "Active"
             ),
-        )
+    )
+
+    def _map_heating(self, device: Dict[str, Any]) -> Dict[str, Any]:
+       return self.build_attribute(
+                device["hiloId"],
+                "Heating",
+                device["power"]["value"] != "0" or device["power"]["value"] is not None,
+            ),
+
+    def _map_power(self, device: Dict[str, Any]) -> Dict[str, Any]:
+        return self.build_attribute( device["hiloId"],"Power", self._powerKwToW(device["power"]["value"], device["power"]["kind"]))
+
+    def _powerKwToW(self, power: float, power_kind: int) -> float:
+        if power_kind == 12: # PowerKind.KW
+            return power * 1000
+
+        return power
 
     def build_attribute(
         self, hilo_id: str, device_attribute: str, value: Any
@@ -157,3 +213,12 @@ class GraphqlValueMapper:
             "value": value,
             "timeStampUTC": datetime.now(timezone.utc).isoformat(),
         }
+
+    def _map_to_floor_mode( self, floor_mode: int) -> str:
+        match floor_mode:
+            case 0:
+                return "Ambient"
+            case 1:
+                return "Floor"
+            case 2:
+                return "Hybrid"
